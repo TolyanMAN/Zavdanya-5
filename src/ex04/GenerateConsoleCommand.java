1f1bee505273ce7a136e7626b9c5b2f0acf7cd42
Максим Герасименко <76556732+TolyanMAN@users.noreply.github.com>
2022-05-02 11:16:48 +0300

    Update GenerateConsoleCommand.java

--- src/ex04/GenerateConsoleCommand.java
+++ src/ex04/GenerateConsoleCommand.java
@@ -3,21 +3,31 @@ import ex02.View;
 /** console command;
  * Generate;
  * pattern Command
- * @author Vlada
+ * @author Maksim
+ 
  * @version 1.0
+ 
  */
 public class GenerateConsoleCommand implements ConsoleCommand {
+    
 
     /** An object that implements an interface {@linkplain View};
+    
      * maintains a collection of objects {@linkplain ex01.Item2d}
+     
      */
     private View view;
+    
 
     /** Initializes a field {@linkplain GenerateConsoleCommand#view}
+    
      * @param view an object that implements an interface {@linkplain View}
+     
      */
     public GenerateConsoleCommand(View view) {
+        
         this.view = view;
+        
     }
 
     @Override
@@ -26,12 +36,15 @@ public class GenerateConsoleCommand implements ConsoleCommand {
     }
     @Override
     public String toString() {
+        
         return "'g'enerate";
     }
     @Override
     public void execute() {
+        
         System.out.println("Random generation.");
         view.viewInit();
+        
         view.viewShow();
     }
 }
