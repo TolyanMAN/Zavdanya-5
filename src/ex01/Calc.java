e532656f7166513334b0f8ad88cc9863e7293cd1
Максим Герасименко <76556732+TolyanMAN@users.noreply.github.com>
2022-05-02 11:06:03 +0300

    Update Calc.java

--- src/ex01/Calc.java
+++ src/ex01/Calc.java
@@ -5,24 +5,31 @@ import java.io.*;
 /**
  * Contains the implementation of methods for calculating and displaying results.
  *
- * @author Vlada
+ * @author Maksim
+ 
  * @version 1.0
+ 
  */
 
 public class Calc {
     /**
      * The name of the file to use during serialization.
+     
      */
     private static final String FNAME = "ex0.Data.bin";
     /**
+    
      * Stores the result of a calculation. class object {@linkplain Item2d}
      */
+    
     private Item2d result;
 
+    
     /**
      * Initializes {@linkplain Calc#result}
      */
     public Calc() {
+        
         result = new Item2d();
     }
 
@@ -51,15 +58,21 @@ public class Calc {
      * @return function evaluation result.
      */
     private int calc(double []arguments) {
+        
         int number = (int)((Math.sin(arguments[0]) + Math.sin(arguments[1]) + Math.sin(arguments[2]) + Math.sin(arguments[3])) / 4.0 * 1000);
+        
         int count = 0;
 
+        
         if(number < 0) {
             number *= -1;
+            
         }
 
         while (number > 0) {
+            
             number &= (number - 1);
+            
             count++;
         }
 
@@ -68,10 +81,13 @@ public class Calc {
 
     /**
      * Calculates the value of a function and saves
+     
      * result in object{@linkplain Calc#result}
+     
      *
      * @param arguments - arguments of the computed function.
      */
+    
     public int init(double []arguments) {
         result.setArguments(arguments);
         return result.setOnesNumber(calc(arguments));
@@ -91,20 +107,28 @@ public class Calc {
      */
     public void save() throws IOException {
         ObjectOutputStream os = new ObjectOutputStream(new
+                                                       
                 FileOutputStream(FNAME));
         os.writeObject(result);
+        
         os.flush();
         os.close();
+        
     }
 
     /**
      * Restores {@linkplain Calc#result} from a file {@linkplain Calc#FNAME}
+     
      *
      * @throws Exception
      */
+    
     public void restore() throws Exception {
+        
         ObjectInputStream is = new ObjectInputStream(new FileInputStream(FNAME));
+        
         result = (Item2d) is.readObject();
+        
         is.close();
     }
 }
