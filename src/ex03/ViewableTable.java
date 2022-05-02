c776cbe5261184ed077bb7f9af8ed0b75e4d2d73
Максим Герасименко <76556732+TolyanMAN@users.noreply.github.com>
2022-05-02 11:11:22 +0300

    Update ViewableTable.java

--- src/ex03/ViewableTable.java
+++ src/ex03/ViewableTable.java
@@ -2,18 +2,26 @@ package ex03;
 import ex02.ViewableResult;
 import ex02.View;
 /** ConcreteCreator
+
  * (design pattern
+ 
  * Factory Method)<br>
+ 
  * Declares a method
+ 
  * "fabricating" objects
- * @author Vlada
+ * @author Maksim
+ 
  * @version 1.0
  * @see ViewableResult
  * @see ViewableTable#getView()
  */
 public class ViewableTable extends ViewableResult {
+    
     /** "fabricating" objects {@linkplain ViewTable} */
+    
     @Override
+    
     public View getView() {
         return new ViewTable();
     }
