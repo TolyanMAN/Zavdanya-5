f7020edcd28021abc577797c5bd73a9291024589
Максим Герасименко <76556732+TolyanMAN@users.noreply.github.com>
2022-05-02 11:18:17 +0300

    Update SaveConsoleCommand.java

--- src/ex04/SaveConsoleCommand.java
+++ src/ex04/SaveConsoleCommand.java
@@ -4,18 +4,24 @@ import ex02.View;
 /** Console command
  * Save;
  * pattern Command
- * @author Vlada
+ * @author Maksim
+ 
  * @version 1.0
  */
+
 public class SaveConsoleCommand implements ConsoleCommand {
+    
 
     /** An object that implements an interface {@linkplain View};
+    
      * maintains a collection of objects {@linkplain ex01.Item2d}
+     
      */
     public View view;
 
     /** Initializes a field {@linkplain SaveConsoleCommand#view}
      * @param view an object that implements an interface {@linkplain View}
+     
      */
     public SaveConsoleCommand(View view) {
         this.view = view;
@@ -30,6 +36,7 @@ public class SaveConsoleCommand implements ConsoleCommand {
         return "'s'ave";
     }
     @Override
+    
     public void execute() {
         System.out.println("Save current.");
         try {
