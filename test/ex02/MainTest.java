f47ba594eb3cf5ba7dfe2462b848c6be7ac1df39
Максим Герасименко <76556732+TolyanMAN@users.noreply.github.com>
2022-05-02 11:04:24 +0300

    Update MainTest.java

--- test/ex02/MainTest.java
+++ test/ex02/MainTest.java
@@ -1,13 +1,16 @@
 package ex02;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+
 import static org.junit.Assert.assertTrue;
+
 import junit.framework.Assert;
 import java.io.IOException;
 import ex01.Item2d;
 /** Performs testing
  * developed classes.
- * @author Vlada
+ * @author Maksim
+ 
  * @version 2.0
  */
 
@@ -20,7 +23,9 @@ public class MainTest {
         Item2d item = new Item2d();
         int ctr = 0;
         item.setOnesNumberAndArguments(7, new double[]{0, 100, 200, 300});
+        
         assertTrue("expected:<" + item + "> but was:<" + view.getItems().get(ctr) + ">",
+                   
                 view.getItems().get(ctr).equals(item));
         ctr++;
         item.setOnesNumberAndArguments(5, new double[]{5, 105, 205, 305});
@@ -32,24 +37,33 @@ public class MainTest {
                 view.getItems().get(ctr).equals(item));
         ctr++;
         item.setOnesNumberAndArguments(5, new double[]{15, 115, 215, 315});
+        
         assertTrue("expected:<" + item + "> but was:<" + view.getItems().get(ctr) + ">",
+                   
                 view.getItems().get(ctr).equals(item));
+        
         ctr++;
         item.setOnesNumberAndArguments(5, new double[]{20, 120, 220, 320});
+        
         assertTrue("expected:<" + item + "> but was:<" + view.getItems().get(ctr) + ">",
                 view.getItems().get(ctr).equals(item));
+        
     }
     /** Serialization check. Correctness of data recovery. */
     @Test
     public void testRestore() {
         ViewResult view1 = new ViewResult(1000);
+        
         ViewResult view2 = new ViewResult();
         // Вычислим значение функции со случайным шагом приращения аргумента
+        
         view1.init(Math.random()*100.0);
         // Сохраним коллекцию view1.items
         try {
+            
             view1.viewSave();
         } catch (IOException e) {
+            
             Assert.fail(e.getMessage());
         }
         // Загрузим коллекцию view2.items
@@ -59,8 +73,10 @@ public class MainTest {
             Assert.fail(e.getMessage());
         }
         // Должны загрузить столько же элементов, сколько сохранили
+        
         assertEquals(view1.getItems().size(), view2.getItems().size());
         // Причем эти элементы должны быть равны.
+        
         // Для этого нужно определить метод equals
         assertTrue("containsAll()", view1.getItems().containsAll(view2.getItems()));
     }
