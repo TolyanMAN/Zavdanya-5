b3ac6187796a2f668180789133e74b9cd21526bf
Максим Герасименко <76556732+TolyanMAN@users.noreply.github.com>
2022-05-02 11:09:58 +0300

    Update ViewableResult.java

--- src/ex02/ViewableResult.java
+++ src/ex02/ViewableResult.java
@@ -2,18 +2,28 @@ package ex02;
 
 /** ConcreteCreator
  * (design pattern
+ 
  * Factory Method)<br>
+ 
  * Declares a method
+ 
  * "fabricating" objects
- * @author Vlada
+ 
+ * @author Maksim
+ 
  * @version 1.0
+ 
  * @see Viewable
+ 
  * @see ViewableResult#getView()
  */
 public class ViewableResult implements Viewable {
+    
     /** Creates a displayable {@linkplain ViewResult} */
+    
     @Override
     public View getView() {
+        
         return new ViewResult();
     }
 
